cab01d5283e7c54e08f0ebf16ad529488cc2ed08
HB0201 <HB0201@HB0201-PC>
2015-12-18 17:44:08 +0900

    fadfa

--- hnb/src/main/java/com/hnb/article/ArticleController.java
+++ hnb/src/main/java/com/hnb/article/ArticleController.java
@@ -2,6 +2,7 @@ package com.hnb.article;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hnb.global.Command;
 import com.hnb.global.CommandFactory;
 
 @Controller
@@ -21,16 +23,19 @@ public class ArticleController {
 	@Autowired ArticleVO article;
 	@Autowired ArticleServiceImpl articleService;
 	
-	@RequestMapping("/list/{pageNo}")
-	public @ResponseBody List<ArticleVO> boardList(
-			@PathVariable("pageNo")String pageNo,			
+	@RequestMapping("/list/{themeNo}/{pageNo}")
+	public @ResponseBody List<ArticleVO> getList(
+			@PathVariable("themeNo")String themeNo,
+			@PathVariable("pageNo")String pageNo,
 			Model model){
 		logger.info("ArticleController-boardList()");
 		logger.info("넘어온 페이지 번호 : {}",pageNo);
-		List<ArticleVO> list = articleService.getList(CommandFactory.list(pageNo));
+		logger.info("넘어온 게시판 테마번호 : {}",themeNo);
+		List<ArticleVO> list = articleService.getList(CommandFactory.boardList(pageNo,themeNo));
 		/*model.addAttribute("memberList", list);
 		model.addAttribute("count", service.count());
 		model.addAttribute("pageNo", pageNo);*/
+		logger.info("검색한 게시글 수 : {}",list.size());
 		return list;
 	}
 	
